6994a5a4e6246dc4b2c5415247b1659cd64d8e9e
ismailgunayy <ismailggunay@gmail.com>
2020-12-03 12:41:12 +0300

    Modified: Init.py and .gitignore
    Added: *.pyc and *.log to .gitignore
    Modified: Init.py, set file paths for Python.

--- Init.py
+++ Init.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import pandas as pd
 import os
-from Note import Note
+from staticFiles.Note import Note
 
 
 def scrapeNotes():
@@ -28,15 +28,15 @@ def scrapeNotes():
         noteList.append(note)
 
     df = pd.DataFrame(noteList)
-    df.to_csv('Notes.csv', index=False, header=False)
+    df.to_csv('.\\staticFiles\\Notes.csv', index=False, header=False)
 
 
 def formatNotes():
 
-    csvFile = open('Notes.csv', 'r')
+    csvFile = open('.\\staticFiles\\Notes.csv', 'r')
     noteList = csvFile.readlines()
-    csvFile = open('Notes.csv', 'w')
-    pyFile = open('ALL_NOTES.py', 'w')
+    csvFile = open('.\\staticFiles\\Notes.csv', 'w')
+    pyFile = open('.\\staticFiles\\ALL_NOTES.py', 'w')
 
     pyFile.write("from Note import Note\n\n")
 
@@ -61,7 +61,7 @@ def formatNotes():
 
     csvFile.close()
     pyFile.close()
-    os.remove("Notes.csv")
+    os.remove(".\\staticFiles\\Notes.csv")
 
 
 if __name__ == "__main__":
